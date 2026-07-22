e8e1b8cb8c8ceb798ba19b09aa1fd03872e64628
Mathieu Brebion <mathieu.brebion@epita.fr>
2026-07-22 19:20:42 +0200

    feat: added non-heterogen support for UnorderedSet hash and equality functions
    also:
    - added helper functions in UnorderedSet to reduce verbosity
    - fixed capacity power of 2 assert of UnorderedSet that was actually checking if capacity was even
    - added missing destructor of UnorderedSet
    - fixed UnorderedSet::remove that was completely wrong

--- src/kernel/utils/UnorderedSet.h
+++ src/kernel/utils/UnorderedSet.h
@@ -6,25 +6,63 @@
 #include "kstddef.h"
 #include "Status.h"
 
-template <typename T, size_t capacity, typename hash_func = Hash<T>, typename equal_func = std::equal_to<T>>
+namespace kdetail
+{
+    template<typename Hash, typename Equal, typename T, typename K>
+    concept FindCompatible =
+        std::invocable<Hash, const K&> &&
+        std::predicate<Equal, const T&, const K&>;
+}
+
+template <typename T, uint32_t capacity, typename hash_func = Hash<T>, typename equal_func = std::equal_to<T>>
 class UnorderedSet
 {
-    static_assert(capacity && capacity % 2 == 0,
-        "HashMap capacity must be a strictly positive power of 2");
+    [[no_unique_address]] hash_func hash;
+    [[no_unique_address]] equal_func equal;
+
+    // Source - https://stackoverflow.com/a/19399478
+    // Posted by sehe
+    // Retrieved 2026-07-22, License - CC BY-SA 3.0
+
+    static constexpr bool is_power_of_2(int v) {
+        return v && ((v & (v - 1)) == 0);
+    }
+
+    static_assert(is_power_of_2(capacity), "HashMap capacity must be a power of 2");
 
     size_t size = 0;
     struct elem
     {
-        alignas(T) char data[sizeof(T)];
-        bool used;
+        alignas(T) std::byte data[sizeof(T)];
+        bool used = false;
+
+        T* ptr() { return std::launder(reinterpret_cast<T*>(data)); }
+
+        const T* ptr() const { return std::launder(reinterpret_cast<const T*>(data)); }
+
+        T& value() { return *ptr(); }
+
+        const T& value() const { return *ptr(); }
     };
     elem elements[capacity];
-
+    template<typename K>
+    requires std::invocable<hash_func, const K&>
+    size_t bucket(const K& key) const;
+    template <typename K>
+    requires kdetail::FindCompatible<hash_func, equal_func, T, K>
+    size_t get_index(const K& element) const;
+    static void advance_index(uint32_t& index);
 public:
+    UnorderedSet() = default;
+    ~UnorderedSet();
+    UnorderedSet(const UnorderedSet&) = delete;
+    UnorderedSet& operator=(const UnorderedSet&) = delete;
     Status add(const T& element);
-    bool is_present(const T& t);
+    bool is_present(const T& t) const;
     void remove(const T& element);
-    Optional<T*> find(const T& element);
+    template <typename K>
+    requires kdetail::FindCompatible<hash_func, equal_func, T, K>
+    Optional<const T*> find(const K& element) const;
 };
 
 #include "UnorderedSet.hxx"
--- src/kernel/utils/UnorderedSet.hxx
+++ src/kernel/utils/UnorderedSet.hxx
@@ -1,26 +1,65 @@
 #include <stdint.h>
 
 #include "Hash.h"
+#include "optional.h"
 #include "UnorderedSet.h"
 
 [[noreturn]]
 extern __attribute__ ((format (printf, 1, 2))) int irrecoverable_error(const char* format, ...);
 
-template <typename T, size_t capacity, typename hash_func, typename equal_func>
+template <typename T, uint32_t capacity, typename hash_func, typename equal_func>
+template<typename K>
+requires std::invocable<hash_func, const K&>
+size_t UnorderedSet<T, capacity, hash_func, equal_func>::bucket(const K& key) const
+{
+    return hash(key) & (capacity - 1);
+}
+
+template <typename T, uint32_t capacity, typename hash_func, typename equal_func>
+template <typename K>
+requires kdetail::FindCompatible<hash_func, equal_func, T, K>
+size_t UnorderedSet<T, capacity, hash_func, equal_func>::get_index(const K& element) const
+{
+    uint32_t index = bucket(element);
+    while (elements[index].used)
+    {
+        if (equal(elements[index].value(), element))
+            return index;
+        advance_index(index);
+    }
+
+    return capacity;
+}
+
+template <typename T, uint32_t capacity, typename hash_func, typename equal_func>
+void UnorderedSet<T, capacity, hash_func, equal_func>::advance_index(uint32_t& index)
+{
+    index = (index + 1) & (capacity - 1);
+}
+
+template <typename T, uint32_t capacity, typename hash_func, typename equal_func>
+UnorderedSet<T, capacity, hash_func, equal_func>::~UnorderedSet()
+{
+    for (auto& e : elements)
+        if (e.used)
+            std::destroy_at(&e.value());
+}
+
+template <typename T, uint32_t capacity, typename hash_func, typename equal_func>
 Status UnorderedSet<T, capacity, hash_func, equal_func>::add(const T& element)
 {
     if (size == capacity)
         return Status::failure();
 
-    uint32_t index = hash_func(element) & (capacity - 1);
+    uint32_t index = bucket(element);
     while (elements[index].used)
     {
-        if (equal_func(elements[index].data))
+        if (equal(const_cast<const T&>(elements[index].value()), element))
             return Status::success(); // Element already present
-        index = (index + 1) & (capacity - 1);
+        advance_index(index);
     }
 
-    *reinterpret_cast<T*>(&elements[index].data) = element;
+    std::construct_at(elements[index].ptr(), element);
     elements[index].used = true;
     size++;
 
@@ -28,49 +67,57 @@ Status UnorderedSet<T, capacity, hash_func, equal_func>::add(const T& element)
 }
 
 
-template <typename T, size_t capacity, typename hash_func, typename equal_func>
-bool UnorderedSet<T, capacity, hash_func, equal_func>::is_present(const T& t)
+template <typename T, uint32_t capacity, typename hash_func, typename equal_func>
+bool UnorderedSet<T, capacity, hash_func, equal_func>::is_present(const T& t) const
 {
-    uint32_t index = hash_func(t) & (capacity - 1);
-    while (elements[index].used)
-    {
-        if (equal_func(reinterpret_cast<T&>(elements[index].data)))
-            return true;
-        index = (index + 1) & (capacity - 1);
-    }
-
-    return false;
+    return get_index(t) != capacity;
 }
 
-template <typename T, size_t capacity, typename hash_func, typename equal_func>
+template <typename T, uint32_t capacity, typename hash_func, typename equal_func>
 void UnorderedSet<T, capacity, hash_func, equal_func>::remove(const T& element)
 {
-    uint32_t index = hash_func(element) & (capacity - 1);
-    while (elements[index].used)
+    uint32_t i = get_index(element);
+    if (i == capacity)
+        irrecoverable_error("%s: element is not present", __PRETTY_FUNCTION__);
+
+    std::destroy_at(&elements[i].value());
+    elements[i].used = false;
+
+    uint32_t j = (i + 1) & (capacity - 1);
+
+    while (elements[j].used)
     {
-        if (equal_func(elements[index].data, element))
+        const uint32_t home = bucket(elements[j].value());
+
+        // Check if element should be shifted back
+        if ((home <= i && i < j) or
+            (i < j && j < home) or
+            (j < home && home <= i))
         {
-            reinterpret_cast<T&>(elements[index].data).~T();
-            elements[index].used = false;
-            return;
+            std::construct_at(elements[i].ptr(), std::move(elements[j].value()));
+            std::destroy_at(&elements[j].value());
+            elements[j].used = false;
+            i = j;
         }
-        index = (index + 1) & (capacity - 1);
+
+        j = (j + 1) & (capacity - 1);
     }
 
-    irrecoverable_error("%s: element is not present", __PRETTY_FUNCTION__);
+    --size;
 }
 
-template <typename T, size_t capacity, typename hash_func, typename equal_func>
-Optional<T*> UnorderedSet<T, capacity, hash_func, equal_func>::find(const T& element)
+
+template <typename T, uint32_t capacity, typename hash_func, typename equal_func>
+template<typename K>
+requires kdetail::FindCompatible<hash_func, equal_func, T, K>
+Optional<const T*> UnorderedSet<T, capacity, hash_func, equal_func>::find(const K& element) const
 {
-    uint32_t index = hash_func(element) & (capacity - 1);
+    uint32_t index = bucket(element);
     while (elements[index].used)
     {
-        if (equal_func(elements[index].data, element))
-        {
-            return {reinterpret_cast<T*>(&element[index].data)};
-        }
-        index = (index + 1) & (capacity - 1);
+        if (equal(elements[index].value(), element))
+            return {elements[index].ptr()};
+        advance_index(index);
     }
 
     return nullopt;
