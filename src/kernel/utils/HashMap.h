a7277d6318e45501e25b2878cd78b198b56442c3
Mathieu Brebion <mathieu.brebion@epita.fr>
2026-07-23 12:36:15 +0200

    feat: added HashMap

---
+++ src/kernel/utils/HashMap.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <functional>
+#include "Hash.h"
+#include "kstddef.h"
+#include "Status.h"
+#include "kdetail.h"
+
+template <typename Key, typename T, uint32_t capacity, typename hash_func = Hash<T>, typename equal_func = std::equal_to<T>>
+class HashMap
+{
+    [[no_unique_address]] hash_func hash;
+    [[no_unique_address]] equal_func equal;
+
+    // Source - https://stackoverflow.com/a/19399478
+    // Posted by sehe
+    // Retrieved 2026-07-22, License - CC BY-SA 3.0
+
+    static constexpr bool is_power_of_2(int v) {
+        return v && ((v & (v - 1)) == 0);
+    }
+
+    static_assert(is_power_of_2(capacity), "HashMap capacity must be a power of 2");
+
+    uint32_t size = 0;
+    struct elem
+    {
+        alignas(T) std::byte data[sizeof(T)];
+        bool used = false;
+
+        T* ptr() { return std::launder(reinterpret_cast<T*>(data)); }
+
+        const T* ptr() const { return std::launder(reinterpret_cast<const T*>(data)); }
+
+        T& value() { return *ptr(); }
+
+        const T& value() const { return *ptr(); }
+    };
+    elem elements[capacity];
+    template<typename K>
+    requires std::invocable<hash_func, const K&>
+    size_t bucket(const K& key) const;
+    template <typename K>
+    requires kdetail::FindCompatible<hash_func, equal_func, T, K>
+    size_t get_index(const K& element) const;
+    static void advance_index(uint32_t& index);
+public:
+    HashMap() = default;
+    ~HashMap();
+    HashMap(const HashMap&) = delete;
+    HashMap& operator=(const HashMap&) = delete;
+    Status add(const T& element);
+    bool is_present(const T& t) const;
+    void remove(const T& element);
+    template <typename K>
+    requires kdetail::FindCompatible<hash_func, equal_func, T, K>
+    Optional<const T*> find(const K& element) const;
+    [[nodiscard]]
+    uint32_t get_size() const;
+    [[nodiscard]]
+    bool is_full() const;
+
+    class Iterator
+    {
+        elem* elems;
+        uint32_t index;
+
+    public:
+        T& operator*() const { return elems[index].value(); }
+        T* operator->() const { return elems[index].ptr(); }
+        Iterator& operator++()
+        {
+            do { ++index; }
+            while (index < capacity && elems[index].used);
+            return *this;
+        }
+        bool operator==(const Iterator& other) const { return other.index == index; }
+
+        Iterator(uint32_t index, elem* elems) : elems(elems), index(index) {}
+    };
+
+    [[nodiscard]]
+    Iterator begin();
+
+    [[nodiscard]]
+    Iterator end();
+};
+
+#include "HashMap.hxx"
---
+++ src/kernel/utils/HashMap.hxx
@@ -0,0 +1,151 @@
+#pragma once
+
+#include "HashMap.h"
+#include <stdint.h>
+#include "optional.h"
+
+[[noreturn]]
+extern __attribute__ ((format (printf, 1, 2))) int irrecoverable_error(const char* format, ...);
+
+template <typename Key, typename T, uint32_t capacity, typename hash_func, typename equal_func>
+template<typename K>
+requires std::invocable<hash_func, const K&>
+size_t HashMap<Key, T, capacity, hash_func, equal_func>::bucket(const K& key) const
+{
+    return hash(key) & (capacity - 1);
+}
+
+template <typename Key, typename T, uint32_t capacity, typename hash_func, typename equal_func>
+template <typename K>
+requires kdetail::FindCompatible<hash_func, equal_func, T, K>
+size_t HashMap<Key, T, capacity, hash_func, equal_func>::get_index(const K& element) const
+{
+    uint32_t index = bucket(element);
+    while (elements[index].used)
+    {
+        if (equal(elements[index].value(), element))
+            return index;
+        advance_index(index);
+    }
+
+    return capacity;
+}
+
+template <typename Key, typename T, uint32_t capacity, typename hash_func, typename equal_func>
+void HashMap<Key, T, capacity, hash_func, equal_func>::advance_index(uint32_t& index)
+{
+    index = (index + 1) & (capacity - 1);
+}
+
+template <typename Key, typename T, uint32_t capacity, typename hash_func, typename equal_func>
+HashMap<Key, T, capacity, hash_func, equal_func>::~HashMap()
+{
+    for (auto& e : elements)
+        if (e.used)
+            std::destroy_at(&e.value());
+}
+
+template <typename Key, typename T, uint32_t capacity, typename hash_func, typename equal_func>
+Status HashMap<Key, T, capacity, hash_func, equal_func>::add(const T& element)
+{
+    if (size == capacity)
+        return Status::failure();
+
+    uint32_t index = bucket(element);
+    while (elements[index].used)
+    {
+        if (equal(const_cast<const T&>(elements[index].value()), element))
+            return Status::success(); // Element already present
+        advance_index(index);
+    }
+
+    std::construct_at(elements[index].ptr(), element);
+    elements[index].used = true;
+    size++;
+
+    return Status::success();
+}
+
+
+template <typename Key, typename T, uint32_t capacity, typename hash_func, typename equal_func>
+bool HashMap<Key, T, capacity, hash_func, equal_func>::is_present(const T& t) const
+{
+    return get_index(t) != capacity;
+}
+
+template <typename Key, typename T, uint32_t capacity, typename hash_func, typename equal_func>
+void HashMap<Key, T, capacity, hash_func, equal_func>::remove(const T& element)
+{
+    uint32_t i = get_index(element);
+    if (i == capacity)
+        irrecoverable_error("%s: element is not present", __PRETTY_FUNCTION__);
+
+    std::destroy_at(&elements[i].value());
+    elements[i].used = false;
+
+    uint32_t j = (i + 1) & (capacity - 1);
+
+    while (elements[j].used)
+    {
+        const uint32_t home = bucket(elements[j].value());
+
+        // Check if element should be shifted back
+        if ((home <= i && i < j) or
+            (i < j && j < home) or
+            (j < home && home <= i))
+        {
+            std::construct_at(elements[i].ptr(), std::move(elements[j].value()));
+            std::destroy_at(&elements[j].value());
+            elements[j].used = false;
+            i = j;
+        }
+
+        j = (j + 1) & (capacity - 1);
+    }
+
+    --size;
+}
+
+
+template <typename Key, typename T, uint32_t capacity, typename hash_func, typename equal_func>
+template <typename K>
+requires kdetail::FindCompatible<hash_func, equal_func, T, K>
+Optional<const T*> HashMap<Key, T, capacity, hash_func, equal_func>::find(const K& element) const
+{
+    uint32_t index = bucket(element);
+    while (elements[index].used)
+    {
+        if (equal(elements[index].value(), element))
+            return {elements[index].ptr()};
+        advance_index(index);
+    }
+
+    return nullopt;
+}
+
+template <typename Key, typename T, uint32_t capacity, typename hash_func, typename equal_func>
+[[nodiscard]]
+uint32_t HashMap<Key, T, capacity, hash_func, equal_func>::get_size() const
+{
+    return size;
+}
+
+template <typename Key, typename T, uint32_t capacity, typename hash_func, typename equal_func>
+bool HashMap<Key, T, capacity, hash_func, equal_func>::is_full() const
+{
+    return size == capacity;
+}
+
+template <typename Key, typename T, uint32_t capacity, typename hash_func, typename equal_func>
+typename HashMap<Key, T, capacity, hash_func, equal_func>::Iterator HashMap<Key, T, capacity, hash_func, equal_func>::
+begin()
+{
+    return Iterator(0, elements);
+}
+
+template <typename Key, typename T, uint32_t capacity, typename hash_func, typename equal_func>
+typename HashMap<Key, T, capacity, hash_func, equal_func>::Iterator HashMap<Key, T, capacity, hash_func, equal_func>::
+end()
+{
+    return Iterator(capacity, elements);
+}
