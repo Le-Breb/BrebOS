84e563b741c526bf4ea2737e39eefdfa0fae9ac8
Mathieu Brebion <mathieu.brebion@epita.fr>
2026-09-25 16:59:55 +0200

    chore: removed outdated Todo

--- src/kernel/core/kmain.cpp
+++ src/kernel/core/kmain.cpp
@@ -32,7 +32,6 @@ extern "C" bool fpu_init_asm_();
 // Todo: more robust TCP stack
 // Todo: optimize ELF loading (investigate all possible sources of poor performance. FAT can be sped up with FAT cache)
 // Todo: add a deallocator for register_physical_data and identity_map and physically_aligned_malloc
-// Todo: check if VFS print_error mechanism could be improved using Result
 // Todo: investigate the segfault that happens after running 'tester'
 extern "C" int kmain(uint ebx) // Ebx contains GRUB's multiboot2 structure pointer
 {
